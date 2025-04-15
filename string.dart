0885f9d2b7e0977b2fba4fb33e72e43db93dde92
shahd2008-alt <almtlqshhd26@gmail.com>
2025-04-15 21:58:48 +0300

    Create string.dart
    from (string) to (int) and (double)

---
+++ string.dart
@@ -0,0 +1,13 @@
+void main() {
+
+// String
+
+  String age = '17';
+
+  print(int.parse(age));
+  print(int.parse(age).runtimeType); // From Strimg to Int => int.parse()
+
+  print(double.parse(age));
+  print(double.parse(age).runtimeType); // From Strimg to double => double.parse()
+
+}
