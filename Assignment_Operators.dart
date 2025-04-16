42daeae7f5db5345e25a4243456909a7d8e469a4
shahd2008-alt <almtlqshhd26@gmail.com>
2025-04-16 20:57:51 +0300

    Create Assignment_Operators.dart
    Assignment Operators

---
+++ Assignment_Operators.dart
@@ -0,0 +1,19 @@
+void main() {
+
+  int a = 0;
+  a += 10; // a = a + 10 = 10
+  print(a);
+
+  int b = 0;
+  b -= 10; // b = b - 10 = -10
+  print(b);
+
+  int c = 10;
+  c *= 20; // c = c * 20 = 200
+  print(c);
+
+  double d = 10;
+  d /= 20; // d = d / 20 = 0.5
+  print(d);
+
+}
