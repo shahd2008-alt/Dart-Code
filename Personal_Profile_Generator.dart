5519a9e1a237c28be050dcaa603ab363c64e7fed
shahd2008-alt <almtlqshhd26@gmail.com>
2025-04-19 04:48:22 +0300

    Create Personal_Profile_Generator.dart
    Personal Profile Generator

---
+++ Personal_Profile_Generator.dart
@@ -0,0 +1,13 @@
+void main() {
+  String name = 'Shahd';
+  int age = 17;
+  double height = 60;
+  String language = 'Dart';
+  bool isFlutterDev = true;
+
+  print('Name: $name');
+  print('Age: $age');
+  print('Height: ${height} inches');
+  print('Favorite Language: $language');
+  print('Flutter Developer: $isFlutterDev');
+}
